8e6485df7d1f83298545a4c725fb23535f0785d9
Mariya Zviagina <68685601+zvgna@users.noreply.github.com>
2022-12-28 23:51:55 +0300

    Add files via upload

---
+++ Lab-3.cpp
@@ -0,0 +1,207 @@
+﻿#include <iostream>
+#include <ctime>
+#include <iomanip>
+#include <Windows.h>
+using namespace std;
+
+const int N = 6;
+
+void Show(int(*A)[N]) {
+    int* end = A[0] + N * N - 1;
+
+    HANDLE hStdout;
+    COORD destCoord;
+    hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+        {
+            destCoord.X = i * 4;
+            destCoord.Y = j;
+            SetConsoleCursorPosition(hStdout, destCoord);
+            cout << A[i][j];
+        }
+}
+
+void ShakeA(int(*A)[N]) {
+    for (int i = 0; i < (N / 2); i++) {
+        for (int j = 0; j < N; j++) {
+            swap(A[i][j], A[i + (N / 2)][j]);
+        }
+    }
+    for (int i = 0; i < (N / 2); i++) {
+        for (int j = 0; j < (N / 2); j++) {
+            swap(A[i][j], A[i + (N / 2)][j + (N / 2)]);
+        }
+    }
+}
+
+void ShakeB(int(*A)[N]) {
+    for (int i = 0; i < (N / 2); i++) {
+        for (int j = 0; j < N; j++) {
+            if (j < (N / 2)) {
+                swap(A[i][j], A[i + (N / 2)][j + (N / 2)]);
+            }
+            else {
+                swap(A[i][j], A[i + (N / 2)][j - (N / 2)]);
+            }
+        }
+    }
+}
+
+void ShakeC(int(*A)[N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < (N / 2); j++) {
+            swap(A[i][j], A[i][j + (N / 2)]);
+        }
+    }
+}
+
+void ShakeD(int(*A)[N]) {
+    for (int i = 0; i < (N / 2); i++) {
+        for (int j = 0; j < N; j++) {
+            swap(A[i][j], A[i + (N / 2)][j]);
+        }
+    }
+}
+
+void Sort(int(*A)[N]) {
+    int* pt = A[0];
+    int nm = N * N - 1;
+    for (int n = 0; n < N*N; ++n) {
+        for (int i = n; i < N*N - 1 - n; ++i) {
+            if (*(pt+i) > *(pt + i + 1))
+                swap(*(pt + i), *(pt + i + 1));
+            if (*(pt + nm - i) < *(pt + nm - i - 1))
+                swap(*(pt + nm - i), *(pt + nm - i - 1));
+        }
+    }
+}
+
+int main()
+{
+    setlocale(0, "");
+    srand(time(0));
+
+    int A[N][N];
+
+     {
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++) {
+                A[i][j] = 0;
+            }
+
+        Show(A);
+
+        int* end = A[0] + N * N - 1; 
+
+        HANDLE hStdout;
+        COORD destCoord;
+        hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+        for (int* pt = A[0], i = 0, x = 0, y = 0; pt <= end + (N - 1); pt++, i++, y++) {
+            if (i % N == 0 && i != 0) {
+                x++;
+                y = 0;
+                if (x % 2 == 0)
+                    pt = &A[x][y];
+                else
+                    pt = &A[x][y + N - 1];
+            }
+            if (x % 2 == 0) {
+                destCoord.X = x * 4;
+                destCoord.Y = y;
+                SetConsoleCursorPosition(hStdout, destCoord);
+                *pt = 1 + rand() % (N * N);
+                cout << *pt;
+                Sleep(50);
+            }
+            else {
+                destCoord.X = x * 4;
+                destCoord.Y = N - y - 1;
+                SetConsoleCursorPosition(hStdout, destCoord);
+                *(pt - y - y) = 1 + rand() % (N * N);
+                cout << *(pt - y - y);
+                Sleep(50);
+            }
+        }
+
+        cout << "\r";
+        for (int i = 1; i <= N; i++)
+            cout << "\n";
+        system("pause");
+    }
+
+    {
+        system("cls");
+        ShakeA(A);
+        Show(A);
+        Sleep(2000);
+
+        system("cls");
+        ShakeB(A);
+        Show(A);
+        Sleep(2000);
+
+        system("cls");
+        ShakeC(A);
+        Show(A);
+        Sleep(2000);
+
+        system("cls");
+        ShakeD(A);
+        Show(A);
+
+        cout << "\n";
+        system("pause");
+    }
+
+    {
+        system("cls");
+        Sort(A);
+        Show(A);
+
+        cout << "\n";
+        system("pause");
+    }
+
+    {
+        int input;
+        string sign;
+
+    Menu: {
+        cout << "Напишите через пробел действие ('+', '-', '*', '/') и на сколько вы хотите его выполнить\n";
+        cout << "Пример: '+ 5'\n";
+        cin >> sign >> input;
+        }
+
+    if (sign == "+") {
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                A[i][j] += input;
+    }
+    else if (sign == "-") {
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                A[i][j] -= input;
+    }
+    else if (sign == "*") {
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                A[i][j] *= input;
+    }
+    else if (sign == "/") {
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                A[i][j] /= input;
+    }
+    else {
+        goto Menu;
+    }
+
+    system("cls");
+    Show(A);
+
+    cout << "\n";
+    }
+}
---
+++ lab-4.cpp
@@ -0,0 +1,153 @@
+﻿#include <iostream>
+#include <fstream> 
+#include <string>
+
+using namespace std;
+
+string text;
+//удаление лишних пробелов
+void DeleteSpace() {
+    for (int i = 0; i < text.size(); i++) {
+        if (text[i] == ' ' && text[i + 1] == ' ')
+            while (text[i + 1] == ' ')
+                text.erase((i + 1), 1);
+    }
+}
+//удаление лишних знаков
+void Unsign() {
+    for (int i = 0; i <= text.size(); i++) {
+        if ((text[i] == '.' || text[i] == ',' || text[i] == ';' || text[i] == ':' || text[i] == '!' || text[i] == '?') && (text[i + 1] == '.' || text[i + 1] == ',' || text[i + 1] == ';' || text[i + 1] == ':' || text[i + 1] == '!' || text[i + 1] == '?'))
+            if (text[i] == '.' && text[i + 1] == '.' && text[i + 2] == '.' && text[i + 3] == ' ')
+                i += 3;
+            else {
+                while (text[i + 1] == '.' || text[i + 1] == ',' || text[i + 1] == ';' || text[i + 1] == ':' || text[i + 1] == '!' || text[i + 1] == '?')
+                    text.erase((i + 1), 1);
+            }
+    }
+}
+//изменение регистра
+void Registr() {
+    for (int i = 1; i < text.size(); i++) {
+        if (text[i] == ' ')
+            i += 2;
+        text[i] = tolower(text[i]);
+    }
+}
+//идз.Вывести на экран ту же последовательность, удалив из всех слов заданный набор букв и (или) цифр.
+void Posled (string txt) {
+    string destr;
+    cout << "\n4. Введите последовательность, которую хотите удалить из каждого слова: ";
+    cin >> destr;
+    for (int i = 0, k = 0; i < txt.size(); i++)
+    {
+        if (text[i] == destr[0]) {
+            k = 1;
+            for (int j = 1; j < destr.size(); j++) 
+                if (destr[j] != txt[i + j]) {
+                    k = 0;
+                    break;
+                }
+            if (k == 1) {
+                while (txt[i] != destr[destr.size() - 1])
+                    txt.erase((i), 1);
+                txt.erase((i), 1);
+                i--;
+            }
+        }
+    }
+    cout << "\nРезультат изменения: " << txt <<"\n\n";
+}
+//линейный поиск
+void Search() {
+    string key;
+    int k;
+    int count = 0;
+
+    cin >> key;
+
+    for (int i = 0; i < text.size(); i++) {
+        if (text[i] == key[0]) {
+            k=1;
+            for (int j = 1; j < key.size(); j++) {
+                if (text[i + j] != key[j]) {
+                    k = 0;
+                    break;
+                }
+            }
+            if (k == 1)
+                count++;
+        }
+    }
+    if (count == 0) {
+        cout << "По запросу ничего не найдено";
+    }
+    else {
+        cout << "Найдено " << count << " соответствий";
+    }
+}
+
+int main()
+{
+    setlocale(0, "");
+
+     {
+        int input;
+    Menu: {
+        cout << "Создать новую последовательность или взять готовую\n" <<
+            "1. Написать новую последовательность\n" <<
+            "2. Взять готовую последовательность\n";
+        cin >> input;
+        }
+    if (input == 1) {
+        cin.ignore();
+        getline(cin, text);
+    }
+    else if (input == 2) { 
+        ifstream in("C:\\Users\\user\\Documents\\!prog\\lab - 4\\text.txt");
+
+        if (in.is_open())
+            getline(in, text);
+        in.close();
+    }
+    else
+        goto Menu;
+    cout << "1. Исходная строка: " << text << "\n";
+    }
+
+     {
+        DeleteSpace();
+        Unsign();
+        Registr();
+
+        cout << "2. Отредактированная строка: " << text << "\n";
+    }
+
+    {
+        cout << "3. Cтрока в обратном порядке: ";
+        int a = 0;
+        text += " ";
+
+        for (int i = text.length(); i >= 0; i--) {
+            if (text[i] == ' ') {
+                for (int k = i + 1; k <= (i + a); k++) {
+                    cout << text[k];
+                }
+                a = 0;
+            }
+            a++;
+        }
+        for (int i = 0; i < text.length(); i++) {
+            if (text[i] == ' ') break;
+            cout << text[i];
+        }
+    }
+
+    {
+        Posled(text);
+    }
+
+    {
+        cout << "5. Введите подстроку, которую вы хотите найти в отредактированной строке: ";
+        Search();
+    }
+}
